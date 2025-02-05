6ba6fd818683ab7d7ff00f5e7a4ac31215834fa9
Kareem Hussen <KareemHussen500@gmail.com>
2025-02-05 21:45:28 +0200

    small fix

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gita</title>
-
-    <!-- Font -->
-    <link href="{{ asset('assets/admin/css/fonts.css') }}" rel="stylesheet">
-
-    <!-- CSS Front Template -->
-    <link rel="stylesheet" href="{{ asset('assets/admin/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/admin/css/style.css') }}">
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
-
-</head>
-<body>
-    
+@extends('layout.main')
 <!-- Table -->
 
+@section('content')
 @if(session('success'))
     <div class="alert alert-success">
         {{ session('success') }}
@@ -52,7 +33,6 @@
     </table>
 
 </div>
-<!-- End Table -->
 
 <script type="text/javascript">
 
@@ -75,6 +55,12 @@
     })
     
 </script>
+
+@endsection
+
+<!-- End Table -->
+
+
 
 </body>
 </html>
