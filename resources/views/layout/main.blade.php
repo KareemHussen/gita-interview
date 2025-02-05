548219cf96c8a5a640baac6d6cad878c27aefc26
Kareem Hussen <KareemHussen500@gmail.com>
2025-02-05 21:18:59 +0200

    trash

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,11 +6,11 @@
     <title>Gita</title>
 
     <!-- Font -->
-    <link href="{{ asset('assets/admin/css/fonts.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/fonts.css') }}" rel="stylesheet"> -->
 
     <!-- CSS Front Template -->
-    <link rel="stylesheet" href="{{ asset('assets/admin/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/admin/css/style.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}"> -->
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
